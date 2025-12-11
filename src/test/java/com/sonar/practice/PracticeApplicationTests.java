0368859b3194cd665a5b43940a28af310032d01b
tejait <teja.chintapalli246@gmail.com>
2025-12-11 16:52:49 +0530

    second commit of jenkins test

--- src/test/java/com/sonar/practice/PracticeApplicationTests.java
+++ src/test/java/com/sonar/practice/PracticeApplicationTests.java
@@ -1,14 +1,17 @@
 package com.sonar.practice;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @SpringBootTest
 class PracticeApplicationTests {
-
+    private static final Logger logger = LoggerFactory.getLogger(PracticeApplicationTests.class);
 	@Test
 	void contextLoads() {
+        logger.info("testing log for jenkins first commit");
 	}
 
 }
