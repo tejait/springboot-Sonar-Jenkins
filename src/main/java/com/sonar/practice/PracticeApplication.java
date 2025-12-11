c1ace793e44a1a25a06a44239c6fd325a60dda5a
tejait <teja.chintapalli246@gmail.com>
2025-12-11 16:56:46 +0530

    third commit to test Jenkins

--- src/main/java/com/sonar/practice/PracticeApplication.java
+++ src/main/java/com/sonar/practice/PracticeApplication.java
@@ -1,13 +1,16 @@
 package com.sonar.practice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeApplication {
-
+    private static final Logger logger = LoggerFactory.getLogger(PracticeApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+        logger.info("Application Started Successfully");
 	}
 
 }
